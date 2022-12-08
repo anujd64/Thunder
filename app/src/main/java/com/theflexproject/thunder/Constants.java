49a12ea9a6da0dffd6ca9e8f30e3e448605d3d3e
anujd64 <89857126+anujd64@users.noreply.github.com>
2022-12-08 09:01:08 +0530

    Update Constants.java

--- app/src/main/java/com/theflexproject/thunder/Constants.java
+++ app/src/main/java/com/theflexproject/thunder/Constants.java
@@ -2,8 +2,24 @@ package com.theflexproject.thunder;
 
 public class Constants {
     public static final String TMDB_GET_REQUEST_BASE_URL ="https://api.themoviedb.org/3/search/movie?api_key=";
+    public static final String TMDB_BASE_URL = "https://api.themoviedb.org/3/";
     public static final String TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
-    public static final String TMDB_BACKDROP_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w780";
+    public static final String TMDB_BACKDROP_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w1280";
+    public static final String FANART_IMAGE_BASE_URL = "https://webservice.fanart.tv/v3/";
     public static final String TMDB_API_KEY = "";
+    public static final String SIMPLE_PROGRAM_DOWNLOAD_API = "https://geolocation.zindex.eu.org/generate.json?id=";
+    public static final String CF_CACHE_TOKEN = "";
+
+    public static String getFanartApiKey(){
+        final String[] KEYS = {
+                "",
+                "",
+                ""};
+        Random random = new Random();
+        int index = random.nextInt(KEYS.length);
+        return KEYS[index];
+    }
+
+
 
 }
